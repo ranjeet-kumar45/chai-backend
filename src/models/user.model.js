96e0697abab413f816bafab7648420f1305e5bfa
ranjeet-kumar45 <ranjeetkushwaha@gmail.com>
2026-09-13 14:41:25 +0530

    fix all register controller bugs

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema(
      },
      watchHistory:[
       {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "Video"
       }
      ],
@@ -49,10 +49,10 @@ const userSchema = new mongoose.Schema(
 )
 
 userSchema.pre("save",async function (next){
-  if(!this.isModified("password")) return next();
+  if(!this.isModified("password")) return next;
 
   this.password = await bcrypt.hash(this.password, 10)
-  next()
+  next
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
